ed38ac15e11886e80a954cc0ddfcc34e3c4070b9
kreizo <thekreizo@gmail.com>
2019-02-19 16:02:52 +0200

    fix bg

--- src/components/MainAnimation/Background.js
+++ src/components/MainAnimation/Background.js
@@ -26,17 +26,28 @@ export class Background extends Component {
       backgroundImage: nextBackgroundImage,
       disableTransition: nextDisableTransition,
       transitionEnd: nextTransitionEnd,
+      backgroundClassName: nextBackgroundClassName,
     },
     nextState,
   ) {
-    const { status, x, y, backgroundImage, disableTransition, transitionEnd } = this.props;
+    const {
+      status,
+      x,
+      y,
+      backgroundImage,
+      disableTransition,
+      transitionEnd,
+      backgroundClassName,
+    } = this.props;
+
     return (
       transitionEnd !== nextTransitionEnd ||
       status !== nextStatus ||
       x !== nextX ||
       y !== nextY ||
       backgroundImage !== nextBackgroundImage ||
-      disableTransition !== nextDisableTransition
+      disableTransition !== nextDisableTransition ||
+      backgroundClassName !== nextBackgroundClassName
     );
   }
 
