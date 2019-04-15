7f7553af82bd7dfc0db6e4b5687ca7caeffdb9cf
Igor <thekreizo@gmail.com>
2019-04-15 19:21:29 +0300

    fixes

--- src/html.js
+++ src/html.js
@@ -73,6 +73,7 @@ class HTML extends React.Component {
         <head>
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
           {headComponents}
           <script
             dangerouslySetInnerHTML={{
