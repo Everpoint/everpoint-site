3d3920f798f3735de9afc69bc1e0313e17efa443
Igor <thekreizo@gmail.com>
2019-04-18 09:59:11 +0300

    politics middle

--- src/styles/politics.js
+++ src/styles/politics.js
@@ -1,40 +1,51 @@
 import styled from "astroturf";
 
 import { Article } from "../components/Elements/Article";
-import { BigH1, H3 } from "../components/Typography/Headlines";
-import { Paragraph } from "../components/Typography/Paragraph";
-import { UnorderedList } from "../components/Typography/UnorderedList";
+import { BigH1 } from "../components/Typography/Headlines";
 
 export const PoliticsContainer = styled(Article)`
-  padding: 15.8571rem 0 10.2857rem 0;
+  padding: 15.8571rem 0 8.8571rem 0;
+  ul {
+    li {
+      margin-bottom: 1.64rem;
+    }
+  }
+  p {
+  }
+
+  @media (max-width: 1199px) {
+    padding: 12.5714rem 0 6.9142rem 0;
+  }
+`;
+
+export const Header = styled("header")`
+  margin-bottom: 9rem;
+  @media (max-width: 1199px) {
+    margin-bottom: 6.7142rem;
+  }
 `;
 
 export const SubTitle = styled("div")`
   color: rgba(38, 44, 55, 0.5);
   margin-bottom: 3.8571rem;
+  @media (max-width: 1199px) {
+    margin-bottom: 3.4rem;
+  }
 `;
 
 export const MainTitle = styled(BigH1)`
   line-height: 1.13;
+  padding-right: 8rem;
 `;
 
-export const P = styled(Paragraph)`
-  margin-bottom: 9.5714rem;
-`;
-
-export const Title = styled(H3)``;
-
-export const Ul = styled(UnorderedList)`
-  margin-bottom: 1.7142rem;
-  li {
-    font-size: 1.2857rem;
-    margin-bottom: 1.74rem;
-    padding-left: 4rem;
-    &:before {
-      width: 8px;
-      height: 8px;
-      top: 0.64rem;
-      margin: 0 0 0 1.8rem;
-    }
+export const Block = styled("div")`
+  margin-bottom: 9rem;
+  @media (max-width: 1199px) {
+    margin-bottom: 6.7142rem;
   }
+`;
+
+export const UpdateDate = styled(SubTitle)`
+  text-align: right;
+  margin-bottom: 0;
 `;
---
+++ src/styles/typography.js
@@ -0,0 +1,99 @@
+import { css } from "astroturf";
+
+const styles = css`
+  .typography {
+    ul {
+      padding: 0;
+      list-style-position: outside;
+      list-style-image: none;
+      margin: 0 0 1.7142rem 0;
+
+      li {
+        font-size: 1.2857rem;
+        line-height: normal;
+        position: relative;
+        list-style-type: none;
+        padding-left: 4rem;
+
+        &:before {
+          width: 8px;
+          height: 8px;
+          top: 0.64rem;
+          margin: 0 0 0 1.8rem;
+          display: inline-block;
+          position: absolute;
+          left: 0;
+          content: "";
+          flex-shrink: 0;
+          background-color: rgba(38, 44, 55, 0.25);
+          border-radius: 50%;
+        }
+
+        &:last-child {
+          margin-bottom: 0;
+        }
+
+        @media (max-width: 1199px) {
+          padding-left: 2rem;
+        }
+        @media (max-width: 991px) {
+          font-size: 0.8571rem;
+        }
+        @media (max-width: 812px) and (orientation: landscape),
+          (max-width: 767px) and (orientation: portrait) {
+          font-size: 0.7142rem;
+          &:before {
+            top: 0.5rem;
+            width: 6px;
+            height: 6px;
+          }
+        }
+      }
+    }
+
+    h3 {
+      line-height: normal;
+      font-size: 1.7142rem;
+      @media (max-width: 991px) {
+        font-size: 1.4285rem;
+      }
+      @media (max-width: 812px) and (orientation: landscape),
+        (max-width: 767px) and (orientation: portrait) {
+        font-size: 1.2857rem;
+      }
+    }
+
+    h5 {
+      font-size: 1.2857rem;
+      line-height: 1.33;
+      @media (max-width: 1199px) {
+        font-size: 1.2857rem;
+        line-height: 1.5;
+      }
+      @media (max-width: 991px) {
+        font-size: 1rem;
+        line-height: normal;
+      }
+    }
+
+    p {
+      font-size: 1.2857rem;
+      line-height: 1.5;
+      margin-bottom: 1.6rem;
+      @media (max-width: 1199px) {
+        font-size: 1.1428rem;
+      }
+      //@media (max-width: 991px) {
+      //  font-size: 1.1428rem;
+      //  margin-bottom: 1.4rem;
+      //}
+      //@media (max-width: 812px) and (orientation: landscape),
+      //  (max-width: 767px) and (orientation: portrait) {
+      //  font-size: 1rem;
+      //  margin-bottom: 1.2rem;
+      //}
+    }
+  }
+`;
+
+export default styles;
