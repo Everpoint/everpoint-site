cb0a1f87ab63922de809f0574aab52fd2eccb2d6
Igor <thekreizo@gmail.com>
2019-04-25 15:37:34 +0300

    img loader

--- src/components/DevelopmentCycle/DevelopmentCycle.js
+++ src/components/DevelopmentCycle/DevelopmentCycle.js
@@ -2,8 +2,10 @@ import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 
 import { getPixelRatioPropName } from "../../utils/utils";
+import { ImagesDownloadListener } from "../../components/ImagesDownloadListener/ImagesDownloadListener";
 import { Figure, ImgContainer, Img, Content, Title, Description } from "./styles";
 import { Separate } from "../Work/Separate/Separate";
+import { backgrounds } from "../MainPageElements/Background";
 
 export class DevelopmentCycle extends PureComponent {
   static propTypes = {
@@ -23,13 +25,17 @@ export class DevelopmentCycle extends PureComponent {
   };
 
   componentDidMount() {
+    this.imageHeight();
+  }
+
+  imageHeight = () => {
     if (this.img) {
       const ratio = window.devicePixelRatio || 1;
       this.setState({
         imgHeight: this.img.naturalHeight / ratio,
       });
     }
-  }
+  };
 
   onImgRef = img => {
     if (img) {
@@ -40,15 +46,14 @@ export class DevelopmentCycle extends PureComponent {
   render() {
     const { imgHeight } = this.state;
     const { odd, name, description, img, offset, Element } = this.props;
+    const image = img[getPixelRatioPropName()];
+
     return (
       <>
         <Figure odd={odd}>
+          <ImagesDownloadListener images={image} onLoad={() => this.imageHeight()} />
           <ImgContainer odd={odd}>
-            <Img
-              src={img[getPixelRatioPropName()]}
-              ref={this.onImgRef}
-              style={{ height: imgHeight }}
-            />
+            <Img src={image} ref={this.onImgRef} style={{ height: imgHeight }} />
           </ImgContainer>
           <Content odd={odd}>
             <Title>{name}</Title>
