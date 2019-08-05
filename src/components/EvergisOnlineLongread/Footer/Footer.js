36a04542a6eed27744045e1e9b3c933e6a3591a4
Igor <thekreizo@gmail.com>
2019-08-05 11:18:13 +0300

    change text

--- src/components/EvergisOnlineLongread/Footer/Footer.js
+++ src/components/EvergisOnlineLongread/Footer/Footer.js
@@ -16,26 +16,8 @@ export const Footer = () => {
       <RightSide>
         <figure>
           Сервис работает в тестовом режиме. Хотите узнать больше и стать одним из первых
-          пользователей EverGIS Online? <br /> <br />
-          <p>
-            <a
-              href="https://docs.google.com/forms/d/e/1FAIpQLSeIX2ahQzW5VRAe8r4Yzm-9tIjETVhuo0-dO5_TwaMPH4TxpA/viewform"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Заполните анкету
-            </a>{" "}
-            <br />и поучаствуйте в <br /> юзабилити-тестировании!
-          </p>
+          пользователей EverGIS Online? Напишите нам в чат, мы предоставим тестовый доступ!
         </figure>
-        {/*<figure>*/}
-        {/*  Хотите узнать больше о возможностях сервиса и протестировать его на ваших реальных*/}
-        {/*  задачах? Нет ничего проще. Зарегистрируйтесь по{" "}*/}
-        {/*  <a href="http://public.everpoint.ru" target="_blank" rel="noopener noreferrer">*/}
-        {/*    ссылке*/}
-        {/*  </a>{" "}*/}
-        {/*  и начните работу уже сегодня.*/}
-        {/*</figure>*/}
       </RightSide>
     </FooterContainer>
   );
