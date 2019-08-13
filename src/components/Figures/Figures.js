feab0a27db2e709fac095261c1853f3d85ab6789
Igor <thekreizo@gmail.com>
2019-08-13 13:31:34 +0300

    figures

--- src/components/Figures/Figures.js
+++ src/components/Figures/Figures.js
@@ -1,15 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Paragraph } from '../../components/Typography/Paragraph';
-import { FiguresSection, Figure } from "./styles";
+import { Paragraph } from "../../components/Typography/Paragraph";
+import { FiguresSection, Figure, IconWrapper } from "./styles";
 
 export const Figures = ({ figures, className }) => {
   return (
     <FiguresSection className={className}>
       {figures.map(({ Icon, text }, index) => (
         <Figure key={`${text}-${index}`}>
-          <Icon />
+          <IconWrapper>
+            <Icon />
+          </IconWrapper>
           <Paragraph>{text}</Paragraph>
         </Figure>
       ))}
