88ad51ad33692a7dbe60da426c8dba2a910ce92f
Igor <thekreizo@gmail.com>
2020-04-20 01:30:19 +0300

    all packages update

--- src/layouts/main.js
+++ src/layouts/main.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Helmet from "react-helmet";
+import { Helmet } from "react-helmet";
 import sortBy from "lodash/sortBy";
 
 import { mergedRoutes } from "../routes/utils";
